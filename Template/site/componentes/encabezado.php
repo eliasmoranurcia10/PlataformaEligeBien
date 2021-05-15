7f35eea30000ce099be302cae80a14e61647ff24
AlexisArteaga13 <alexisarteagacarhua@gmail.com>
2021-05-14 23:50:29 -0500

    Arteaga

---
+++ Template/site/componentes/encabezado.php
@@ -0,0 +1,48 @@
+<header class="section page-header rd-navbar-transparent-wrap">
+            <!--RD Navbar-->
+            <div class="rd-navbar-wrap">
+                <nav class="rd-navbar rd-navbar-transparent" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="20px" data-xl-stick-up-offset="20px" data-xxl-stick-up-offset="20px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+                    <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+                    <div class="rd-navbar-aside-outer rd-navbar-collapse">
+                        <div class="rd-navbar-aside">
+                            <div class="rd-navbar-info">
+                                <div class="icon novi-icon mdi mdi-phone"></div><a href="tel:#">+51 936 193 965</a>
+                            </div>
+                            <ul class="list-lined">
+                                <li><a href="#">Inicia sesión</a></li>
+                                <li><a href="#">Crer una cuenta</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="rd-navbar-main-outer">
+                        <div class="rd-navbar-main-inner">
+                            <div class="rd-navbar-main">
+                                <!--RD Navbar Panel-->
+                                <div class="rd-navbar-panel">
+                                    <!--RD Navbar Toggle-->
+                                    <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                                    <!--RD Navbar Brand-->
+                                    <div class="rd-navbar-brand">
+                                        <!--Brand--><a class="brand" href="index.html"><img class="brand-logo-dark" src="images/logo-default-293x44.png" alt="" width="146" height="22" /><img class="brand-logo-light" src="images/logo-inverse-310x44.png" alt="" width="155" height="22" /></a>
+                                    </div>
+                                </div>
+                                <div class="rd-navbar-main-element">
+                                    <div class="rd-navbar-nav-wrap">
+                                        <ul class="rd-navbar-nav">
+                                            <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Inicio</a>
+                                            </li>
+                                            <li class="rd-nav-item"><a class="rd-nav-link" href="about.html">Acerca de</a>
+                                            </li>
+                                            <li class="rd-nav-item"><a class="rd-nav-link" href="typography.html">Typography</a>
+                                            </li>
+                                            <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contáctanos</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
--- Template/site/index.php
+++ Template/site/index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html class="wide wow-animation" lang="es">
-
-<head>
-    <title>Elige Bien</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/fonts.css">
-    <link rel="stylesheet" href="css/style.css">
-    <style>
-        .ie-panel {
-            display: none;
-            background: #212121;
-            padding: 10px 0;
-            box-shadow: 3px 3px 5px 0 rgba(0, 0, 0, .3);
-            clear: both;
-            text-align: center;
-            position: relative;
-            z-index: 1;
-        }
-
-        html.ie-10 .ie-panel,
-        html.lt-ie-10 .ie-panel {
-            display: block;
-        }
-    </style>
-</head>
+<?php include("./componentes/header.php") ?>
 
 <body>
     <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
@@ -49,54 +19,7 @@
         </div>
     </div>
     <div class="page"><a class="banner banner-top" href="https://www.templatemonster.com/website-templates/monstroid2.html" target="_blank"><img src="images/monstroid.jpg" alt="" height="0"></a>
-        <header class="section page-header rd-navbar-transparent-wrap">
-            <!--RD Navbar-->
-            <div class="rd-navbar-wrap">
-                <nav class="rd-navbar rd-navbar-transparent" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="20px" data-xl-stick-up-offset="20px" data-xxl-stick-up-offset="20px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
-                    <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
-                    <div class="rd-navbar-aside-outer rd-navbar-collapse">
-                        <div class="rd-navbar-aside">
-                            <div class="rd-navbar-info">
-                                <div class="icon novi-icon mdi mdi-phone"></div><a href="tel:#">+51 936 193 965</a>
-                            </div>
-                            <ul class="list-lined">
-                                <li><a href="#">Inicia sesión</a></li>
-                                <li><a href="#">Crer una cuenta</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="rd-navbar-main-outer">
-                        <div class="rd-navbar-main-inner">
-                            <div class="rd-navbar-main">
-                                <!--RD Navbar Panel-->
-                                <div class="rd-navbar-panel">
-                                    <!--RD Navbar Toggle-->
-                                    <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
-                                    <!--RD Navbar Brand-->
-                                    <div class="rd-navbar-brand">
-                                        <!--Brand--><a class="brand" href="index.html"><img class="brand-logo-dark" src="images/logo-default-293x44.png" alt="" width="146" height="22" /><img class="brand-logo-light" src="images/logo-inverse-310x44.png" alt="" width="155" height="22" /></a>
-                                    </div>
-                                </div>
-                                <div class="rd-navbar-main-element">
-                                    <div class="rd-navbar-nav-wrap">
-                                        <ul class="rd-navbar-nav">
-                                            <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Inicio</a>
-                                            </li>
-                                            <li class="rd-nav-item"><a class="rd-nav-link" href="about.html">Acerca de</a>
-                                            </li>
-                                            <li class="rd-nav-item"><a class="rd-nav-link" href="typography.html">Typography</a>
-                                            </li>
-                                            <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contáctanos</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </header>
+       <?php include("./componentes/encabezado.php")?>
         <!--Swiper-->
         <section class="section swiper-container swiper-slider swiper-slider-1 context-dark" data-loop="true" data-autoplay="5000" data-simulate-touch="false">
             <div class="swiper-wrapper">
@@ -318,7 +241,7 @@
                 <div class="row">
                     <div id="becas" class="row"></div>
                 </div>
-
+                      <a href="#" class="btn btn-success">Ver más</a>
             </div>
 
         </section>
@@ -455,12 +378,14 @@
                     html += '<h5 class="card-title ">' + data[i].Convocatoria + '</h5>';
                     html += '<p class="card-text">Becas ofertadas: ' + data[i].Becas_ofertadas + '</p>';
                     html += '<p class="card-text">Modalidad : ' + data[i].ModalidadDescripcion + '</p>';
+                    html += '<p class="card-text"><a target="_blank" href="https://www.pronabec.gob.pe/beca-18-2021/">Ver en Pronabec</a></p>';
+                  
                     html += '</div>';
                     html += '</div>';
                     console.log(html);
                     $("#becas").append(html);
                 }
-                console.log("despueshtm" + html);
+              
 
                 //  console.log(data[0].Apiguid);
             });
